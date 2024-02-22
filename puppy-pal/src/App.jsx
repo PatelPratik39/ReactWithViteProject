dcecf830ef433a1e37432e7f3e10142329089133
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-22 13:52:52 -0600

    rendered selected pupy list

--- puppy-pal/src/App.jsx
+++ puppy-pal/src/App.jsx
@@ -1,27 +1,53 @@
 // import PuppyList from "./PuppyList.jsx";
 import { puppyList } from "./data.js";
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 function App() {
   console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
-  // console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // function handleClick() {
+  //   setPuppies()
+  // }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       <div>
         <h1 className="header"> 🐶 Puppy List 🐶</h1>
       </div>
+
       <div className="puppyList">
         {puppies.map((puppy) => {
           return (
-            <p key={puppy.id}>
+            <p
+              key={puppy.id}
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+            >
               <li>
-                Name : {puppy.name} - Age :  {puppy.age}
+                🆔 : {puppy.name}
               </li>
             </p>
           );
         })}
+      </div>
+
+      <div className="newList">
+        <h2> Your Puppy Choice : </h2>
+        🐶 🆔 ➡{" "}
+        {featPupId && (
+          <div>
+            <h3>{featuredPup.name}</h3>
+            <ul className="ulList">
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
