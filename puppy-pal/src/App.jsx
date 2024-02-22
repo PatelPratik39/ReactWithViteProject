fa85c5f66ebb3c048443d38193d33ebb78d70a93
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-22 13:23:24 -0600

    puppy List rendered

--- puppy-pal/src/App.jsx
+++ puppy-pal/src/App.jsx
@@ -1,7 +1,28 @@
+// import PuppyList from "./PuppyList.jsx";
+import { puppyList } from "./data.js";
+import { useState } from "react";
+import './App.css'
+
 function App() {
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  // console.log(puppies);
   return (
     <>
-      <h1>Vite + React</h1>
+      <div>
+        <h1 className="header"> 🐶 Puppy List 🐶</h1>
+      </div>
+      <div className="puppyList">
+        {puppies.map((puppy) => {
+          return (
+            <p key={puppy.id}>
+              <li>
+                Name : {puppy.name} - Age :  {puppy.age}
+              </li>
+            </p>
+          );
+        })}
+      </div>
     </>
   );
 }
---
+++ puppy-pal/src/PuppyList.jsx
@@ -0,0 +1,7 @@
+const PuppyList = () => {
+    return ( <div>
+        <h1>Puppy List</h1>
+    </div> );
+}
+ 
+export default PuppyList;
